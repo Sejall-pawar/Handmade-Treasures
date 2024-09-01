6ae914196fec163382093cd668ab24ccc46935e4
Sejall-pawar <sejalpawar463@gmail.com>
2024-09-01 15:27:56 +0530

    added the conatct page changes

--- src/Views/Contact/contact.js
+++ src/Views/Contact/contact.js
@@ -13,20 +13,20 @@ function contact() {
       <div className='contact-container'>
       <div className='sub-container'>
       <h1 className='info-head' style={{color : THEME.primary}}>Contact Information:</h1>
-      <h2 className='contact-info'>Email: support@handmadetreasures.com</h2>
-      <h2 className='contact-info'>Phone: +123-456-7890</h2>
-      <h2 className='contact-info'>Address: 123 Artisan Lane, Craft City, CA 12345</h2>
+      <h2 className='contact-info'>Email: <a href="mailto:support@handmadetreasures.com">support@homemadetreasures.com</a></h2>
+      <h2 className='contact-info'>Phone no.<a href="tel:+919860312523">+91 9860312523</a></h2>
+      <h2 className='contact-info'>Address: Sinnar, Nashik, Maharashtra</h2>
       </div>
 
       <div className='sub-container'>
       <h1 className='info-head' style={{color : THEME.primary}}>Follow Us on Social Media:</h1>
-      <h2 className='contact-info'>Facebook: Handmade Treasures</h2>
-      <h2 className='contact-info'>Instagram: @handmadetreasures</h2>
-      <h2 className='contact-info'>Pinterest: Handmade Treasures</h2>
+      <h2 className='contact-info'>Facebook: Homemade Treasures</h2>
+      <h2 className='contact-info'>Instagram: @homemadetreasures</h2>
+      <h2 className='contact-info'>Pinterest: Homemade Treasures</h2>
       </div>
       </div>
 
-      <h1 className='thankyou'>Thank you for visiting Handmade Treasures! We look forward to assisting you.</h1>
+      <h1 className='thankyou'>Thank you for visiting Homemade Treasures! We look forward to assisting you.</h1>
 
       <Footer/>
     </div>
